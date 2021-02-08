33710d2c1b936eb7035d8528ffc44dbf948141d1
Alexispoveda <alexis.mars20@hotmail.com>
2021-02-07 22:05:49 -0500

    Add greetings file

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 8000;
 const app = express();
 
 app.get('/', (request, response)=> {
-    response.send('<h1>Hello World</h1>');
+    response.sendFile('./greetings.html');
 });
 
 app.listen(PORT);
